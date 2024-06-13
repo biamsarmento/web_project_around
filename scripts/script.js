f3dfc1f15940f8cae6accdd29ee62d70f334a7d3
biamsarmento <biamsarmento@gmail.com>
2024-06-13 12:12:48 -0300

    Correções Finais

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,18 @@
-let content = document.querySelector(".content");
-let editButton = content.querySelector(".profile__info-edit-button");
-let profileName = content.querySelector(".profile__info-title");
-let profileActivity = content.querySelector(".profile__info-activity");
-let submitButton = content.querySelector(".popup__submit-button");
-let closePopupButton = content.querySelector(".popup__close-button");
-let popup = content.querySelector(".popup");
-let likes = content.querySelectorAll(".card__tag-like");
+const content = document.querySelector(".content");
+const editButton = content.querySelector(".profile__info-edit-button");
+const profileName = content.querySelector(".profile__info-title");
+const profileActivity = content.querySelector(".profile__info-activity");
+const submitButton = content.querySelector(".popup__submit-button");
+const closePopupButton = content.querySelector(".popup__close-button");
+const popup = content.querySelector(".popup");
+const likes = content.querySelectorAll(".card__tag-like");
+const form = content.querySelector(".form");
 
 function handlePopup() {
   popup.classList.toggle("popup_opened");
 
-  let nome = document.querySelector(".form__input_type_name");
-  let atividade = document.querySelector(".form__input_type_activity");
+  const nome = document.querySelector(".form__input_type_name");
+  const atividade = document.querySelector(".form__input_type_activity");
 
   if (nome.value == "" || atividade.value == "") {
 
@@ -20,24 +21,22 @@ function handlePopup() {
   }
 }
 
-function handleSaveChanges() {
+function handleSaveChanges(evt) {
 
-  let nome = document.querySelector(".form__input_type_name");
-  let atividade = document.querySelector(".form__input_type_activity");
+  evt.preventDefault();
 
-  if (nome.value != "" && atividade.value != "") {
-    profileName.textContent = nome.value;
-    profileActivity.textContent = atividade.value;
-    handlePopup();
-  }
-  else {
-    alert("Preencha os campos!");
-  }
+  const nome = document.querySelector(".form__input_type_name");
+  const atividade = document.querySelector(".form__input_type_activity");
+
+  profileName.textContent = nome.value;
+  profileActivity.textContent = atividade.value;
+
+  handlePopup();
 }
 
 editButton.addEventListener('click', handlePopup);
 closePopupButton.addEventListener('click', handlePopup);
-submitButton.addEventListener('click', handleSaveChanges);
+form.addEventListener('submit', handleSaveChanges)
 
 likes.forEach(like => {
   like.addEventListener('click', () => {
